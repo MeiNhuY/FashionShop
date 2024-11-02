720bd49f075766b402de07a8e04a237f95cb31ac
MeiNhuY <ymtn.23it@vku.udn.vn>
2024-11-02 19:59:45 +0700

    first commit

---
+++ views/cart/cart.php
@@ -0,0 +1,149 @@
+
+
+
+  <div class="page-heading bg-light">
+    <div class="container">
+      <div class="row align-items-end text-center">
+        <div class="col-lg-7 mx-auto">
+          <h1>Giỏ hàng</h1>  
+          <p class="mb-4"><a href="index.html">Trang chủ</a> / <strong>Giỏ hàng</strong></p>        
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+  <div class="untree_co-section">
+    <div class="container">
+      <div class="row mb-5">
+        <form class="col-md-12" method="post">
+          <div class="site-blocks-table">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th class="product-thumbnail">Hình ảnh</th>
+                  <th class="product-name">Sản phẩm</th>
+                  <th class="product-price">Giá</th>
+                  <th class="product-quantity">Số lượng</th>
+                  <th class="product-total">Tổng tiền</th>
+                  <th class="product-remove">Xóa</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="product-thumbnail">
+                    <img src="images/products/jacket-1-min.jpg" alt="Image" class="img-fluid">
+                  </td>
+                  <td class="product-name">
+                    <h2 class="h5 text-black">Top Up T-Shirt</h2>
+                  </td>
+                  <td>$49.00</td>
+                  <td>
+                    <div class="input-group mb-3" style="max-width: 120px;">
+                      <div class="input-group-prepend">
+                        <button class="btn btn-outline-black js-btn-minus" type="button">&minus;</button>
+                      </div>
+                      <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                      <div class="input-group-append">
+                        <button class="btn btn-outline-black js-btn-plus" type="button">&plus;</button>
+                      </div>
+                    </div>
+
+                  </td>
+                  <td>$49.00</td>
+                  <td><a href="#" class="btn btn-black btn-sm">X</a></td>
+                </tr>
+
+                <tr>
+                  <td class="product-thumbnail">
+                    <img src="images/products/bottoms-1-min.jpg" alt="Image" class="img-fluid">
+                  </td>
+                  <td class="product-name">
+                    <h2 class="h5 text-black">Polo Shirt</h2>
+                  </td>
+                  <td>$49.00</td>
+                  <td>
+                    <div class="input-group mb-3" style="max-width: 120px;">
+                      <div class="input-group-prepend">
+                        <button class="btn btn-outline-black js-btn-minus" type="button">&minus;</button>
+                      </div>
+                      <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                      <div class="input-group-append">
+                        <button class="btn btn-outline-black js-btn-plus" type="button">&plus;</button>
+                      </div>
+                    </div>
+
+                  </td>
+                  <td>$49.00</td>
+                  <td><a href="#" class="btn btn-black btn-sm">X</a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </form>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row mb-5">
+            <div class="col-md-6 mb-3 mb-md-0">
+              <button class="btn btn-black btn-sm btn-block">Cập nhật giỏ hàng</button>
+            </div>
+            <div class="col-md-6">
+              <button class="btn btn-outline-black btn-sm btn-block">Quay lại cửa hàng</button>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <label class="text-black h4" for="coupon">Mã giảm giá</label>
+              <p>Nhập mã giảm giá của bạn nếu có</p>
+            </div>
+            <div class="col-md-8 mb-3 mb-md-0">
+              <input type="text" class="form-control py-3" id="coupon" placeholder="Nhập mã giảm giá tại đây">
+            </div>
+            <div class="col-md-4">
+              <button class="btn btn-black">Áp dụng</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 pl-5">
+          <div class="row justify-content-end">
+            <div class="col-md-7">
+              <div class="row">
+                <div class="col-md-12 text-right border-bottom mb-5">
+                  <h3 class="text-black h4 text-uppercase">Chi tiết đơn hàng</h3>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col-md-6">
+                  <span class="text-black">Subtotal</span>
+                </div>
+                <div class="col-md-6 text-right">
+                  <strong class="text-black">$230.00</strong>
+                </div>
+              </div>
+              <div class="row mb-5">
+                <div class="col-md-6">
+                  <span class="text-black">Total</span>
+                </div>
+                <div class="col-md-6 text-right">
+                  <strong class="text-black">$230.00</strong>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <!-- <button class="btn btn-black btn-lg py-3 btn-block" onclick="window.location='?act=checkout.php'">Mua hàng</button> -->
+                  <button class="btn btn-black btn-lg py-3 btn-block" href="?act=checkout.php" aria-label="Mua hàng">Mua hàng</button>
+
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
