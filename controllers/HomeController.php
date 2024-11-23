5a7fe671d3d3f92c140c38806262c9ffa895b18a
thanhtuyenie <phanthithanhtuyen0107@gmail.com>
2024-11-23 10:50:13 +0700

    tuyen-sp-best-seller

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -18,18 +18,17 @@ class HomeController
             $data_chitietDM[$i] = $this->home_model->getCategoryDetails($i);
         }
 
-        $data_limit1 = $this->home_model->limit(0,4);
-        $data_limit2 = $this->home_model->limit(4,4);
-        $data_limit3 = $this->home_model->limit(8,4);
-        $data_limit4 = $this->home_model->limit(12,4);
-        $data_arr = array($data_limit1,$data_limit2,$data_limit3,$data_limit4);
-        $data_random = $this->home_model->getProductTypes(2);
+        $data_limit1 = $this->home_model->limit(0,10);
+
+        $data_arr = array($data_limit1);
+
+        $data_random = $this->home_model->getRandomProducts(2);
 
         $data_banner = $this->home_model->getBanners(0,2);
 
-        $data_sanpham1 = $this->home_model->getProductsByCategory(0,8,1);
-        $data_sanpham2 = $this->home_model->getProductsByCategory(0,8,2);
-        $data_sanpham3 = $this->home_model->getProductsByCategory(0,8,3);
+        $data_sanpham1 = $this->home_model->getLatestProducts(10);
+
+        $data_sp_best_seller= $this->home_model->getBestSellingProducts(6);
 
         require_once('views/index.php');
     }
